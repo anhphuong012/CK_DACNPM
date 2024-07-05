faeaef5f8eee6cc9736a1524f9ddd68d3c2138a8
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-07-05 20:53:09 +0700

    update forgot pass

--- frontend/project/src/component/user/page/ForgotPass.js
+++ frontend/project/src/component/user/page/ForgotPass.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../css/forgotPassword.css';
+import BackIcon from '../img/back.svg';
 
 function ForgotPass() {
   return (
     <div id='forgot-password'>
       <div className='box_forPass'>
+        <a className='forPass_back' href='/login'>
+          <img src={BackIcon} alt='back home' className='forPass_back-icon'/>
+        </a>
         <p className='forPass-title'>Quên mật khẩu</p>
         <form className='forPass-form'>
           <input
